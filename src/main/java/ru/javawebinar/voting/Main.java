0c3376b459d8f80a9c06ec33d3f3ea816ac4094e
Vasiliy Golban <vgolban@accesssoftek.com>
2020-01-06 11:46:48 +0200

    Update initDB, populateDB sql scripts;

--- src/main/java/ru/javawebinar/voting/Main.java
+++ src/main/java/ru/javawebinar/voting/Main.java
@@ -1,16 +1,10 @@
 package ru.javawebinar.voting;
 
-import org.hsqldb.rights.User;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import ru.javawebinar.voting.model.Dish;
-import ru.javawebinar.voting.repository.JdbcDishRepository;
 import ru.javawebinar.voting.web.DishRestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,7 +20,7 @@ public class Main {
             appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             DishRestController dishController = appCtx.getBean(DishRestController.class);
-            dishController.create(new Dish("hello", "2019-12-24 10:00:00", 100000));
+            dishController.create(new Dish("hello", "2019-12-24 10:00:00", 100002));
             System.out.println();
 
 //            MealRestController mealController = appCtx.getBean(MealRestController.class);
