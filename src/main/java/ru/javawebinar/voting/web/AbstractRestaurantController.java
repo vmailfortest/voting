4c24098995615fd18ece559fb54ee9d4ec541794
Vasiliy Golban <vgolban@accesssoftek.com>
2020-01-09 22:49:26 +0200

    Add logging to AbstractRestaurantController;

--- src/main/java/ru/javawebinar/voting/web/AbstractRestaurantController.java
+++ src/main/java/ru/javawebinar/voting/web/AbstractRestaurantController.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.voting.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.voting.model.Restaurant;
 import ru.javawebinar.voting.service.RestaurantService;
@@ -7,19 +9,19 @@ import ru.javawebinar.voting.service.RestaurantService;
 import java.util.List;
 
 public abstract class AbstractRestaurantController {
-    //    private static final Logger log = LoggerFactory.getLogger(RestaurantRestController.class);
+        private static final Logger log = LoggerFactory.getLogger(RestaurantRestController.class);
 
     @Autowired
     private RestaurantService service;
 
     public Restaurant create(Restaurant restaurant) {
-//        log.info("create from to {}", restaurant);
+        log.info("create restaurant {}", restaurant);
         return service.create(restaurant);
     }
 
     public Restaurant get(int id) {
 //        int userId = SecurityUtil.authUserId();
-//        log.info("get meal {} for user {}", id, userId);
+        log.info("get restaurant {}", id);
         return service.get(id, 0);
     }
 
@@ -28,11 +30,12 @@ public abstract class AbstractRestaurantController {
     }
 
     public void update(Restaurant restaurant, int id) {
-//        log.info("create from to {}", restaurant);
+        log.info("update restaurant {}", restaurant);
         service.update(restaurant);
     }
 
     public void delete(int id) {
+        log.info("delete restaurant {}", id);
         service.delete(id);
     }
 }
